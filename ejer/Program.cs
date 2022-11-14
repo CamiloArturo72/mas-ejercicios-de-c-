b9a7c7503d7c39f9c1a8c585922182d57d6d4641
artur <artur@AOURUS-CAMILO>
2022-11-14 18:21:44 -0500

    Agregar archivos de proyecto.

---
+++ ejer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Dynamic;
+
+namespace ejercicios
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int numero;
+            int resultado;
+            int doblado;
+
+            Console.WriteLine("Por favor ingresa un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            doblado = numero = 2;
+            resultado = doblado = 25;
+            Console.WriteLine($"El resultado de la operacion es : {resultado}");
+        }
+    }
